5d31f8f0779952b225c28fa405babf05183549b6
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2017-06-24 23:47:10 +0900

    Parameterize routes

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,27 @@
 module Main where
 
 import           Data.Aeson
-import qualified Data.Text          as T
+import qualified Data.ByteString.Lazy   as BS
+import qualified Data.Text              as T
 import           Web.Spock
 import           Web.Spock.Config
 
 main :: IO ()
 main = do
-    dummyDB <- readFile "db.json"
-    spockCfg <- defaultSpockCfg () PCNoDatabase dummyDB
-    runSpock 8080 $ spock spockCfg $ do
+    eDummyDB <- loadDummyDB "db.json"
+    case eDummyDB of
+        Left message -> do
+            putStrLn message
+        Right dummyDB -> do
+            spockCfg <- defaultSpockCfg () PCNoDatabase dummyDB
+            runSpock 8080 $ spock spockCfg $ do
 
-        get root $ do
-            db <- getState
-            text $ T.pack db
+                let route = ("sample" :: T.Text)
+                get (static $ T.unpack route) $ do
+                    text $ T.concat [ "Hello, ", route, "!" ]
+
+type DummyDB = Object  -- HashMap Text Value
+type Route = T.Text
+
+loadDummyDB :: FilePath -> IO (Either String DummyDB)
+loadDummyDB fp = eitherDecode' <$> BS.readFile fp

d3bbddfcbc8f94c9c1848e6f0db1f907f69753c6
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2017-06-29 03:40:40 +0900

    Use lenses instead of the bare aeson datatypes

--- src/Web/DummyMe/DB.hs
+++ src/Web/DummyMe/DB.hs
@@ -8,29 +8,24 @@ module Web.DummyMe.DB (
     , selectById
     ) where
 
+import           Control.Lens
 import           Data.Aeson
-import qualified Data.HashMap.Lazy      as HM
+import           Data.Aeson.Lens
 import qualified Data.Text              as T
 import qualified Data.Text.IO           as TIO
-import qualified Data.Vector            as V
 
 type DummyDB = T.Text
 type TopLevelKey = T.Text
-type EntityId = Int
+type EntityId = Integer
 
 loadDummyDB :: FilePath -> IO DummyDB
 loadDummyDB = TIO.readFile
 
 select :: TopLevelKey -> DummyDB -> (DummyDB, Maybe Value)
-select x db = undefined
+select x db = (db, db ^? key x)
 
 selectById :: TopLevelKey -> EntityId -> DummyDB -> (DummyDB, Maybe Value)
-selectById key id db =
-    case select key db of
-        (_, Just (Array records)) -> (db, V.find (idIs id) records)
-        (_, _) -> (db, Nothing)
+selectById x n db = (db, db ^? key x ^.. folded . filtered (idIs n) ^? ix 0)
 
 idIs :: EntityId -> Value -> Bool
-idIs idNum (Object obj) =
-    HM.lookup "id" obj == Just (Number (fromIntegral idNum))
-idIs _ _ = False
+idIs n val = val ^? key "id" . _Integer == Just n

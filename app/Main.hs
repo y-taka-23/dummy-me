fc6ab7c48b4dcae064ea227bcf0f8a85a6f1a028
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2017-07-17 04:51:48 +0900

    Extract the definition of routes

--- app/Main.hs
+++ app/Main.hs
@@ -13,20 +13,19 @@ main = do
     appCfg <- getConfig
     dummyDB <- loadDummyDB $ file appCfg
     spockCfg <- mkSpockCfg dummyDB
-    runSpock (port appCfg) $ spock spockCfg $ do
-
-        get var $ \key -> getHandler key
-        get (var <//> var) $ \key id -> getByIdHandler key id
-
-        delete (var <//> var) $ \key id -> deleteByIdHandler key id
-
-        post var $ \key -> postHandler key
-
-        put var $ \key -> putHandler key
-        put (var <//> var) $ \key id -> putByIdHandler key id
+    runSpockNoBanner (port appCfg) $ spock spockCfg routes
 
 mkSpockCfg :: DummyDB -> IO (SpockCfg () () InMemoryDB)
 mkSpockCfg initDB = do
     dbRef <- newIORef initDB
     cfg <- defaultSpockCfg () PCNoDatabase (InMemoryDB dbRef)
     return $ cfg { spc_errorHandler = errorHandler }
+
+routes :: SpockCtxM ctx conn sess InMemoryDB ()
+routes = do
+    get     var           $ \key    -> getHandler key
+    get    (var <//> var) $ \key id -> getByIdHandler key id
+    delete (var <//> var) $ \key id -> deleteByIdHandler key id
+    post    var           $ \key    -> postHandler key
+    put     var           $ \key    -> putHandler key
+    put    (var <//> var) $ \key id -> putByIdHandler key id

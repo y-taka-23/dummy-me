80766c8e8c9327b14a84e5f2f7277af8f6103be8
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2017-07-17 15:10:14 +0900

    Extract the entrypoint of Spock

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,7 @@
 module Main where
 
-import Web.DummyMe.DB
-import Web.DummyMe.Handler
+import Web.DummyMe
 import Web.DummyMe.Config
 
-import Data.IORef
-import Web.Spock        hiding ( file )
-import Web.Spock.Config
-
 main :: IO ()
-main = do
-    appCfg <- getConfig
-    dummyDB <- loadDummyDB $ file appCfg
-    spockCfg <- mkSpockCfg dummyDB
-    runSpockNoBanner (port appCfg) $ spock spockCfg routes
-
-mkSpockCfg :: DummyDB -> IO (SpockCfg () () InMemoryDB)
-mkSpockCfg initDB = do
-    dbRef <- newIORef initDB
-    cfg <- defaultSpockCfg () PCNoDatabase (InMemoryDB dbRef)
-    return $ cfg { spc_errorHandler = errorHandler }
-
-routes :: SpockCtxM ctx conn sess InMemoryDB ()
-routes = do
-    get     var           $ \key    -> getHandler key
-    get    (var <//> var) $ \key id -> getByIdHandler key id
-    delete (var <//> var) $ \key id -> deleteByIdHandler key id
-    post    var           $ \key    -> postHandler key
-    put     var           $ \key    -> putHandler key
-    put    (var <//> var) $ \key id -> putByIdHandler key id
+main = getConfig >>= runDummyMe
---
+++ src/Web/DummyMe.hs
@@ -0,0 +1,32 @@
+module Web.DummyMe (
+      runDummyMe
+    ) where
+
+import Web.DummyMe.DB
+import Web.DummyMe.Config
+import Web.DummyMe.Handler
+
+import Data.IORef
+import Web.Spock        hiding ( file )
+import Web.Spock.Config
+
+runDummyMe :: Config -> IO ()
+runDummyMe appCfg = do
+    dummyDB <- loadDummyDB $ file appCfg
+    spockCfg <- mkSpockCfg dummyDB
+    runSpockNoBanner (port appCfg) $ spock spockCfg routes
+
+mkSpockCfg :: DummyDB -> IO (SpockCfg () () InMemoryDB)
+mkSpockCfg initDB = do
+    dbRef <- newIORef initDB
+    cfg <- defaultSpockCfg () PCNoDatabase (InMemoryDB dbRef)
+    return $ cfg { spc_errorHandler = errorHandler }
+
+routes :: SpockCtxM ctx conn sess InMemoryDB ()
+routes = do
+    get     var           $ \key    -> getHandler key
+    get    (var <//> var) $ \key id -> getByIdHandler key id
+    delete (var <//> var) $ \key id -> deleteByIdHandler key id
+    post    var           $ \key    -> postHandler key
+    put     var           $ \key    -> putHandler key
+    put    (var <//> var) $ \key id -> putByIdHandler key id
